5c3e7418a58264d5342996fd73de6d2c277d39c0
SainSinner <g.cvyat@gmail.com>
2022-10-29 16:22:19 +0500

    123

--- index.js
+++ index.js
@@ -11,25 +11,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
     if(text === '/start') {
-        await bot.sendMessage(chatId, 'Заполните форму выбрав кнопку "каталог" в клавиатуре', {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Каталог', web_app: {url: webAppUrl + '/form'}}]
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
                 ]
             }
         })
 
-
-        await bot.sendMessage(chatId, 'Наш сайт доступен по ссылке ниже', {
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Сайт', web_app: {url: webAppUrl}}]
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         })
@@ -38,14 +36,14 @@ bot.on('message', async (msg) => {
     if(msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
-            await bot.sendMessage(chatId, 'Данные которые вы ввели представлены ниже')
-            await bot.sendMessage(chatId, 'Страна: ' + data?.country);
-            await bot.sendMessage(chatId, 'Улица: ' + data?.street);
-
-            setTimeout( async () => {
-                await bot.sendMessage(chatId, 'Вся информация представлена в этом чате');
-            }, 1000)
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
         } catch (e) {
             console.log(e);
         }
@@ -53,7 +51,7 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products, totalPrice} = req.body;
+    const {queryId, products = [], totalPrice} = req.body;
     try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
@@ -63,14 +61,12 @@ app.post('/web-data', async (req, res) => {
                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
-        return res.status(200).json({});    
+        return res.status(200).json({});
     } catch (e) {
-        return res.status(500).json({});  
+        return res.status(500).json({})
     }
 })
 
-
 const PORT = 8000;
 
-
-app.listen(PORT, () => console.log('Server started on PORT ' + PORT))
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
